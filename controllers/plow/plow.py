0c065be641f9404cb227424670025db2c9637117
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-04-20 15:00:26 -0600

    manual controller done

--- controllers/plow/plow.py
+++ controllers/plow/plow.py
@@ -1,40 +1,53 @@
 """plow controller."""
-from controller import Robot
+from controller import Robot, Keyboard
 from vehicle import Car, Driver
 
 # create the Robot instance.
 driver = Driver()
-
-print(driver.getType())
-camera = driver.getDevice("camera")
-
 # get the time step of the current world
 TIME_STEP = int(driver.getBasicTimeStep())
+
+camera = driver.getDevice("camera")
 camera.enable(TIME_STEP)
+keyboard = driver.getKeyboard()
+keyboard.enable(TIME_STEP)
 
-MAX_SPEED = 10 #km/h?
 
+
+
+speed = 10 #km/h?
+mode = 'manual'
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while driver.step() != -1:
-	current_steer_angle = driver.getSteeringAngle()
-	turn_angle = 0.17 #radians 
 
+	steer_angle = driver.getSteeringAngle()
+
+	turn_angle = 0.17 #	radians, turn 10 degrees each press
 	if mode == 'manual':
 		key = keyboard.getKey()
 		while(keyboard.getKey() != -1): pass
 		if key == keyboard.LEFT:
-			new_angle = current_steer_angle - turn_angle
+			steer_angle -= turn_angle
+			if(steer_angle<= -0.631):
+				print(steer_angle)
+				steer_angle = -0.5
+				print("steer_angle after "+str(steer_angle))
 		elif key == keyboard.RIGHT:
-			new_angle = current_steer_angle + turn_angle
+			steer_angle += turn_angle
+			if(steer_angle>=0.631):
+				steer_angle = 0.5
 		elif key == keyboard.UP:
-			
+			speed+=10
+			if speed >175:
+				speed = 175
 		elif key == keyboard.DOWN:
-
+			speed-=10
+			if speed <-50:
+				speed = -50
 		elif key == ord(' '):
-			vL = 0
-			vR = 0
-  
-	driver.setCruisingSpeed()	
-	print(camera.getImage())
+			speed = 0
+	driver.setSteeringAngle(steer_angle)
+	driver.setCruisingSpeed(speed)	
+	#print(camera.getImage())
 # Enter here exit cleanup code.
