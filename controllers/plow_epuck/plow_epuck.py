a63185711a5e72edee99c7a5c87bbbeb375482af
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-04-26 20:04:50 -0600

    chamges

--- controllers/plow_epuck/plow_epuck.py
+++ controllers/plow_epuck/plow_epuck.py
@@ -14,7 +14,7 @@ MAX_SPEED = 6.28
 
 
 TIME_STEP = int(robot.getBasicTimeStep())
-WHITE_OBJECT_THRESHOLD = 90  # white_pixels
+WHITE_OBJECT_THRESHOLD = 30  # white_pixels
 GREEN_OBJECT_THRESHOLD = 450  # green_pixels
 REACHED_TARGET_THRESHOLD = 0.1 #m
 camera = robot.getDevice("camera")
@@ -29,7 +29,8 @@ right_speed = 0  # rad/s?
 #mode = 'manual'
 #mode = 'snow_seeker'
 #mode = 'inverse_kinematics'
-mode = 'wall_avoidance'
+#mode = 'wall_avoidance'
+mode = 'stupid_plow'
 leftMotor = robot.getDevice("left wheel motor")
 rightMotor = robot.getDevice("right wheel motor")
 leftMotor.setPosition(float('inf'))
@@ -40,22 +41,21 @@ pose_x = 0
 pose_y = 0
 pose_theta = 0
 
-
-
 figure_8_path = [ (-0.37, -0.35), (0.4,-0.4),(0.4,0.4), (-0.4,0.4)]
 current_target = 0
 #target_x = figure_8_path[current_target][0]
 #target_y = figure_8_path[current_target][1]
-target_y = 0.4
-target_x = 0.4
+target_y = 0.35
+target_x = 0.35
+TIMER_COUNT = 0
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(TIME_STEP) != -1:
 
 	gps_x = gps.getValues()[0]
 	gps_y = gps.getValues()[2]
 	
-	print(gps_x)
-	print(gps_y)
+	#print("x "+str(gps_x))
+	#print("y "+str(gps_y))
 	if mode == 'manual':
 		key = keyboard.getKey()
 		while (keyboard.getKey() != -1): pass
@@ -97,7 +97,7 @@ while robot.step(TIME_STEP) != -1:
 				white_count += 1
 			if red < 50 and green > 150 and blue < 50:
 				green_count += 1
-	print("white count chunk 1 "+str(white_count))
+	#print("white count chunk 1 "+str(white_count))
 	#print("grey count chunk 1 " + str(green_count))
 	if (white_count > WHITE_OBJECT_THRESHOLD):
 		white_list[0] = True
@@ -114,7 +114,7 @@ while robot.step(TIME_STEP) != -1:
 				white_count += 1
 			if red < 50 and green > 150 and blue < 50:
 				green_count += 1
-	print("white count chunk 2 "+str(white_count))
+	#print("white count chunk 2 "+str(white_count))
 	#print("grey count chunk 2 " + str(green_count))
 	if (white_count > WHITE_OBJECT_THRESHOLD):
 		white_list[1] = True
@@ -131,7 +131,7 @@ while robot.step(TIME_STEP) != -1:
 				white_count += 1
 			if red < 50 and green > 150 and blue < 50:
 				green_count += 1
-	print("white count chunk 3 "+str(white_count))
+	#print("white count chunk 3 "+str(white_count))
 	#print("grey count chunk 3 " + str(green_count))
 	if (white_count > WHITE_OBJECT_THRESHOLD):
 		white_list[2] = True
@@ -144,9 +144,9 @@ while robot.step(TIME_STEP) != -1:
 	if mode == 'inverse_kinematics':		
 		# Inverse kinematics
 		# error
-		print("Seeking snow")
-		rho = math.sqrt( (target_y- gps_y)**2  +  (target_x-gps_x)**2 )
-		alpha = math.atan2(target_y-gps_y, target_x -gps_x) + pose_theta
+		#print("Seeking snow")
+		rho = math.sqrt( (target_y- pose_y)**2  +  (target_x-pose_x)**2 )
+		alpha = math.atan2(target_y-pose_y, target_x -pose_x) + pose_theta
 
 		if rho < REACHED_TARGET_THRESHOLD:
 			current_target+=1
@@ -181,7 +181,7 @@ while robot.step(TIME_STEP) != -1:
 		right_speed = (2* pos_scalar + angular_scalar*EPUCK_AXLE_DIAMETER)/2
 
 		#print("alpha: "+str(alpha)+"\nRho: "+str(rho))
-	if white_list[1] == True:
+	"""if white_list[1] == True:
 		# drive toward snow
 		mode = 'inverse_kinematics'
 	elif white_list[0] == True:
@@ -189,12 +189,12 @@ while robot.step(TIME_STEP) != -1:
 		mode = 'inverse_kinematics'
 	elif white_list[2] == True:
 		# turn right
-		mode = 'inverse_kinematics'
+		mode = 'inverse_kinematics'"""
 
 
 	if mode == 'wall_avoidance':
 		# map navigation
-		print("Avoiding walls")
+		#print("Avoiding walls")
 		if green_list[1] == True and green_list[0] == True:
 			# wall, turn right
 			right_speed = -MAX_SPEED
@@ -203,12 +203,34 @@ while robot.step(TIME_STEP) != -1:
 			# go straight
 			right_speed = MAX_SPEED
 			left_speed = MAX_SPEED
+	if mode == 'stupid_plow':
+		left_speed = MAX_SPEED / 2
+		right_speed =-MAX_SPEED / 2
 
+		if white_list[1] == True:
+			#push snow forward for 1 second			
+			left_speed = MAX_SPEED
+			right_speed = MAX_SPEED
+			leftMotor.setVelocity(left_speed)
+			rightMotor.setVelocity(right_speed)
+			for i in range(0,100):
+				robot.step(TIME_STEP)
+			left_speed = -MAX_SPEED
+			right_speed = -MAX_SPEED
+			leftMotor.setVelocity(left_speed)
+			rightMotor.setVelocity(right_speed)
+			for i in range(0,100):
+				robot.step(TIME_STEP)
 
-	if (abs(right_speed) > MAX_SPEED):
+	if right_speed > MAX_SPEED:
 		right_speed = MAX_SPEED
-	if (abs(left_speed) > MAX_SPEED):
+	if left_speed > MAX_SPEED:
 		left_speed = MAX_SPEED
+	if right_speed < -MAX_SPEED:
+		right_speed = -MAX_SPEED
+	if left_speed < -MAX_SPEED:
+		left_speed = MAX_SPEED
+
 	leftMotor.setVelocity(left_speed)
 	rightMotor.setVelocity(right_speed)
 
