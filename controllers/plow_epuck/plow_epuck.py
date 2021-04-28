bd3dc513e63d0e979dff5726720a02097879047b
raptorgenix <49776913+raptorgenix@users.noreply.github.com>
2021-04-28 12:51:55 -0600

    working

--- controllers/plow_epuck/plow_epuck.py
+++ controllers/plow_epuck/plow_epuck.py
@@ -17,7 +17,7 @@ MAX_SPEED = 6.28
 
 
 TIME_STEP = int(robot.getBasicTimeStep())
-WHITE_OBJECT_THRESHOLD = 12  # white_pixels
+WHITE_OBJECT_THRESHOLD = 15  # white_pixels
 GREEN_OBJECT_THRESHOLD = 1300  # green_pixels
 REACHED_TARGET_THRESHOLD = 0.01 #m
 camera = robot.getDevice("camera")
@@ -44,6 +44,7 @@ rightMotor.setVelocity(0.0)
 pose_x = 0
 pose_y = 0
 pose_theta = 0
+last_pose_theta = 0
 
 figure_8_path = [ (-0.37, -0.35), (0.4,-0.4),(0.4,0.4), (-0.4,0.4)]
 current_target = 0
@@ -97,31 +98,35 @@ while robot.step(TIME_STEP) != -1:
 	width = camera.getWidth()
 	height = camera.getHeight()
 	chunk_pixels = width / 3 * height / 2
+	divider = int(pose_theta / 6.28) * 5
+	greenheight = False
+	
+	whitenum = WHITE_OBJECT_THRESHOLD - divider
 
 	for x in range(0, int(width / 3)):  # 1st chunk of image
 		for y in range(0, int(height)):  
 			red = image[x][y][0]
 			green = image[x][y][1]
 			blue = image[x][y][2]
-			if red > 240 and green > 240 and blue > 240 and (abs(red-blue-green) <6):
+			if red > 22 and green > 220 and blue > 220 and (abs(red-blue-green) <6):
 				white_count += 1
 			if red < 50 and green > 150 and blue < 50:
 				green_count += 1
 	#print("white count chunk 1 "+str(white_count))
 	#print("grey count chunk 1 " + str(green_count))
 	white_counts.append(white_count)
-	if (white_count > WHITE_OBJECT_THRESHOLD):
+	if (white_count > whitenum):
 		white_list[0] = True
 	if (green_count > GREEN_OBJECT_THRESHOLD):
 		green_list[0] = True
 	white_count = 0
 	green_count = 0
 	for x in range(int(width / 3), int(2 / 3 * width)):  # 2nd chunk of image
-		for y in range(0, 40):  
+		for y in range(0, 44):  
 			red = image[x][y][0]
 			green = image[x][y][1]
 			blue = image[x][y][2]
-			if red > 200 and green > 200 and blue > 200 and (abs(red-blue-green) <6):
+			if red > 220 and green > 220 and blue > 220 and (abs(red-blue-green) <6):
 				white_count += 1
 			if red < 50 and green > 150 and blue < 50:
 				green_count += 1
@@ -129,14 +134,14 @@ while robot.step(TIME_STEP) != -1:
 			red = image[x][y][0]
 			green = image[x][y][1]
 			blue = image[x][y][2]
-			if red > 240 and green > 240 and blue > 240:
+			if red > 220 and green > 220 and blue > 220:
 				white_count += 1
 			if red < 50 and green > 150 and blue < 50:
 				green_count += 1
 	#print("white count chunk 2 "+str(white_count))
 	white_counts.append(white_count)
 	#print("grey count chunk 2 " + str(green_count))
-	if (white_count > WHITE_OBJECT_THRESHOLD):
+	if (white_count > whitenum):
 		white_list[1] = True
 	if (green_count > GREEN_OBJECT_THRESHOLD):
 		green_list[1] = True
@@ -147,14 +152,24 @@ while robot.step(TIME_STEP) != -1:
 			red = image[x][y][0]
 			green = image[x][y][1]
 			blue = image[x][y][2]
-			if red > 240 and green > 240 and blue > 240:
+			if red > 220 and green > 220 and blue > 220:
 				white_count += 1
 			if red < 50 and green > 150 and blue < 50:
 				green_count += 1
 	#print("white count chunk 3 "+str(white_count))
 	#print("grey count chunk 3 " + str(green_count))
+
+
+	for x in range(0, width):
+		for y in range (0, 10):
+			red = image[x][y][0]
+			green = image[x][y][1]
+			blue = image[x][y][2]
+			if red < 20 and green > 210 and blue < 20:
+				greenheight = True
+
 	white_counts.append(white_count)
-	if (white_count > WHITE_OBJECT_THRESHOLD):
+	if (white_count > whitenum):
 		white_list[2] = True
 	if (green_count > GREEN_OBJECT_THRESHOLD):
 		green_list[2] = True
@@ -162,6 +177,9 @@ while robot.step(TIME_STEP) != -1:
 	#print(green_list)
 
 	# can now program controller based off camera data
+
+	
+
 	if mode == 'inverse_kinematics':		
 		# Inverse kinematics
 		# error
@@ -201,12 +219,15 @@ while robot.step(TIME_STEP) != -1:
 		left_speed = MAX_SPEED / 2
 		right_speed =-MAX_SPEED / 2
 
-		if white_list[1] == True:
+		if white_list[1] == True and not abs(last_pose_theta-pose_theta) < .3:
+			if abs(last_pose_theta-pose_theta) < .1:
+				print("Same theta found")
 			mode = 'push_snow'
 
-
+	if mode == 'stop':
+		print("stopped")
 	if mode == 'push_snow':
-		if green_list[1] == True:
+		if greenheight == True:
 			mode = 'reverse'
 		else:
 			left_speed = MAX_SPEED
@@ -214,7 +235,10 @@ while robot.step(TIME_STEP) != -1:
 			push_snow_count+=1
 	if mode == 'reverse':
 		if(push_snow_count == 0):
+			if (whitenum < 3):
+				mode = 'stop'
 			mode = 'stupid_plow'
+			last_pose_theta = pose_theta
 		else:
 			left_speed = -MAX_SPEED
 			right_speed = -MAX_SPEED
@@ -243,5 +267,6 @@ while robot.step(TIME_STEP) != -1:
 	pose_y -= ds * math.sin(pose_theta)
 	pose_x -= ds * math.cos(pose_theta)
 	pose_theta -= (dsr - dsl) / EPUCK_AXLE_DIAMETER
+	
 	#print("X: %f Y: %f Theta: %f " % (pose_x, pose_y, pose_theta))
 # Enter here exit cleanup code.
